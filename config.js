cf2b9b14de1a5933c4d6faf55eb98b380f508bd7
maximo2012 <juan.c.cortes@gmail.com>
2021-07-02 00:49:09 -0500

    Update config.js

--- config.js
+++ config.js
@@ -2,10 +2,10 @@ const env = process.env;
 
 const config = {
   db: { /* don’t expose password or any sensitive info, done only for demo */
-    host: env.DB_HOST || 'freedb.tech',
-    user: env.DB_USER || 'freedbtech_geshann',
-    password: env.DB_PASSWORD || 'G2VjjQ5d47zyjqX',
-    database: env.DB_NAME || 'freedbtech_languagesabc',
+    host: env.DB_HOST || 'solutex.cloudaccess.host',
+    user: env.DB_USER || 'wnuqdyrb',
+    password: env.DB_PASSWORD || '160577',
+    database: env.DB_NAME || 'wnuqdyrb',
   },
   listPerPage: env.LIST_PER_PAGE || 10,
 };
