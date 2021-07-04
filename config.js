907c999e6b2029ab910b890966fa9c6f8001cd36
maximo2012 <juan.c.cortes@gmail.com>
2021-07-03 22:48:25 -0500

    Update config.js

--- config.js
+++ config.js
@@ -2,10 +2,10 @@ const env = process.env;
 
 const config = {
   db: { /* don’t expose password or any sensitive info, done only for demo */
-    host: env.DB_HOST || 'solutex.cloudaccess.host',
-    user: env.DB_USER || 'wnuqdyrb',
-    password: env.DB_PASSWORD || '160577',
-    database: env.DB_NAME || 'wnuqdyrb',
+    host: env.DB_HOST || '',
+    user: env.DB_USER || '',
+    password: env.DB_PASSWORD || '',
+    database: env.DB_NAME || '',
   },
   listPerPage: env.LIST_PER_PAGE || 10,
 };
